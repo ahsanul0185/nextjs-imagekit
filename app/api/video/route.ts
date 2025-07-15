dc927912f9f904246864dd38cf2834f077b43a65
Ahsanul Haque <ahsanul0185@gmail.com>
2025-07-15 20:10:16 +0600

    first commit

---
+++ app/api/video/route.ts
@@ -0,0 +1,60 @@
+import { connectDB } from "@/lib/db"
+import Video from "@/models/Video"
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { NextRequest } from "next/server";
+import { IVideo } from "@/models/Video";
+
+export async function GET() {
+    try {
+        await connectDB()
+        
+        const videos = await Video.find({}).sort({createdAt : -1}).lean();
+
+        if (!videos || videos.length === 0) {
+            return NextResponse.json([] ,{status : 200})
+        }
+
+        return NextResponse.json(videos)
+
+    } catch (error) {
+         return NextResponse.json({error: "Failed to fetch videos"}, {status : 500})
+    }
+}
+
+
+export async function POST(request : NextRequest) {
+    try {
+       const session = await getServerSession(authOptions)
+
+       if (!session) {
+         return NextResponse.json({error: "Unauthorized"}, {status : 401})
+       }
+
+       await connectDB();
+
+       const body : IVideo = await request.json();
+
+       if (!body.title || !body.description || !body.videoUrl || !body.thumbnailUrl) {
+        return NextResponse.json({error: "Missing required fields"}, {status : 400})
+       }
+
+       const videoData = {
+        ...body,
+        controls : body?.controls ?? true,
+        transformation : {
+            height : 1920,
+            width : 1080,
+            quality: body?.transformation?.quality ?? 100
+        }
+       }
+
+       const newVideo = await Video.create(videoData)
+
+       return NextResponse.json(newVideo);
+
+    } catch (error) {
+         return NextResponse.json({error: "Failed to create new video"}, {status : 500})
+    }
+}
